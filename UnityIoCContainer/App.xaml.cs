afe7d134084f42afc1c2a09c9b8ac48d47b553dd
Quilv <levanqui88@gmail.com>
2020-07-30 15:25:53 +0700

    + init

---
+++ UnityIoCContainer/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityIoCContainer.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UnityIoCContainer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ProductsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ UnityIoCContainer/Views/ProductsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UnityIoCContainer.Views
+{
+    public partial class ProductsPage : ContentPage
+    {
+        public ProductsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
